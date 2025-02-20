48911545734389709df6b5b25c24894d847395de
jangirsamarthiqol <samarth@truestate.in>
2025-02-21 00:14:54 +0530

    added navbar & loader

--- rev-roar/src/App.jsx
+++ rev-roar/src/App.jsx
@@ -1,28 +1,33 @@
 // src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import  { useState, useEffect } from 'react';
+import Loader from './components/Loader';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Location from './components/Location';
-import Gallery from './components/Gallery';
-import Enquiry from './components/Enquiry';
-import AboutUs from './components/AboutUs';
-import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <Navbar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/location" element={<Location />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/enquiry" element={<Enquiry />} />
-          <Route path="/about-us" element={<AboutUs />} />
-        </Routes>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          {/* Other routes/components */}
+        </>
+      )}
+    </>
   );
 }
 
